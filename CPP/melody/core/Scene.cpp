3e36e38e0c7ab65bac91688a36e48d439ab6e26a
Ferdian <ferdian.artwork@gmail.com>
2022-05-15 05:49:31 +0700

    first init

---
+++ CPP/melody/core/Scene.cpp
@@ -0,0 +1,23 @@
+#include "melody.hpp"
+
+using namespace melody;
+
+Scene::Scene() {
+
+}
+
+Scene::~Scene() {
+	destroy();
+}
+
+void Scene::initialize() {
+
+}
+
+void Scene::update() {
+
+}
+
+void Scene::destroy() {
+
+}
